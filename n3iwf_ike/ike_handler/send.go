beb58d8d6d430d794cbc02145d9d7fdb9af8bc34
syujy <yu35756.cs04@nctu.edu.tw>
2020-03-03 19:15:10 +0800

    Fix cycling import error

--- n3iwf_ike/ike_handler/send.go
+++ n3iwf_ike/ike_handler/send.go
@@ -1,19 +1,19 @@
-package ike_message
+package ike_handler
 
 import (
 	"gofree5gc/src/n3iwf/n3iwf_handler/n3iwf_message"
+	"gofree5gc/src/n3iwf/n3iwf_ike/ike_message"
 	"gofree5gc/src/n3iwf/n3iwf_ike/udp_server"
 )
 
-func SendIKEMessageToUE(ueSendInfo *n3iwf_message.UDPSendInfoGroup, message *IKEMessage) {
+func SendIKEMessageToUE(ueSendInfo *n3iwf_message.UDPSendInfoGroup, message *ike_message.IKEMessage) {
 	ikeLog.Trace("[IKE] Send IKE message to UE")
 	ikeLog.Trace("[IKE] Encoding...")
-	pkt, err := Encode(message)
+	pkt, err := ike_message.Encode(message)
 	if err != nil {
 		ikeLog.Errorln(err)
 		return
 	}
 	ikeLog.Trace("[IKE] Sending...")
 	udp_server.Send(ueSendInfo, pkt)
-	return
 }
