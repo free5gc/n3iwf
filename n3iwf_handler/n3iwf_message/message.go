8ed90d6239ccfebb3923da3c334b1295007a90a5
syujy <yu35756.cs04@nctu.edu.tw>
2020-03-11 17:17:43 +0800

    Modify comment

--- n3iwf_handler/n3iwf_message/message.go
+++ n3iwf_handler/n3iwf_message/message.go
@@ -8,7 +8,7 @@ type HandlerMessage struct {
 	Event       Event
 	UDPSendInfo *UDPSendInfoGroup // used only when Event == EventN1UDPMessage
 	SCTPAddr    string            // used when Event == EventNGAPMessage || Event == EventSCTPConnectMessage
-	UEInnerIP   string            // used only Event == EventN1TunnelCPMessage || Event == EventN1TunnelUPMessage
+	UEInnerIP   string            // used when Event == EventN1TunnelCPMessage || Event == EventN1TunnelUPMessage
 	TEID        uint32            // used only when Event == EventGTPMessage
 	Value       interface{}
 }
