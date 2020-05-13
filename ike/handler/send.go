3215daabac4d94f69b5f3f5918dc30cc7c5e75d6
syujy <yu35756.cs04@nctu.edu.tw>
2020-05-13 17:28:48 +0800

    style(n3iwf): Follow Go coding style

---
+++ ike/handler/send.go
@@ -0,0 +1,25 @@
+package handler
+
+import (
+	n3iwf_message "free5gc/src/n3iwf/handler/message"
+	ike_message "free5gc/src/n3iwf/ike/message"
+	"free5gc/src/n3iwf/ike/udp_server"
+)
+
+func SendIKEMessageToUE(ueSendInfo *n3iwf_message.UDPSendInfoGroup, message *ike_message.IKEMessage) {
+	ikeLog.Trace("[IKE] Send IKE message to UE")
+	ikeLog.Trace("[IKE] Encoding...")
+	pkt, err := ike_message.Encode(message)
+	if err != nil {
+		ikeLog.Errorln(err)
+		return
+	}
+	// As specified in RFC 7296 section 3.1, the IKE message send from/to UDP port 4500
+	// should prepend a 4 bytes zero
+	if ueSendInfo.ChannelID == udp_server.ChannelIDForPort4500 {
+		prependZero := make([]byte, 4)
+		pkt = append(prependZero, pkt...)
+	}
+	ikeLog.Trace("[IKE] Sending...")
+	udp_server.Send(ueSendInfo, pkt)
+}
